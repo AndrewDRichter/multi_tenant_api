fc0bb82145c25eb55fe91859eae6622cf9fce22c
AndrewDRichter <andrewrichter1@hotmail.com>
2025-07-23 17:09:25 -0300

    ok

--- client_app/views.py
+++ client_app/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Brands
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>Welcome to the tenant page</h1>')
+    brands = Brands.objects.all()
+    context = {
+        'brands': brands,
+    }
+    return render(request, 'index.html', context)
 
-def brand_create(request, name):
-    brand = Brands(name=name)
-    brand.save()
-    return HttpResponse(f'<h1>Brand: {brand.name} created.</h1>')
+def brand_create(request):
+    if request.POST:
+        name = request.POST.get('name')
+        brand = Brands(name=name)
+        brand.save()
+        return redirect('index')
